ce911d5fdad80a534cd42583b77a29ff51745af2
shiba10222 <thilder10222@gmail.com>
2024-08-13 00:33:21 +0800

    砍lowdb

--- routes/prod.js
+++ routes/prod.js
@@ -1,31 +1,31 @@
 import { Router } from "express";
 import multer from "multer";
 // import conn from '../db.js';
-import {Low} from "lowdb";
-import { JSONFile } from "lowdb/node";
+// import {Low} from "lowdb";
+// import { JSONFile } from "lowdb/node";
 import {resolve} from 'path';
 
-const filePath = resolve(import.meta.dirname, "../data/prod/newProduct.json");
+// const filePath = resolve(import.meta.dirname, "../data/prod/newProduct.json");
 const router = Router();
-const defaultData = [];
-const db = new Low(new JSONFile(filePath), defaultData);
-await db.read();
+// const defaultData = [];
+// const db = new Low(new JSONFile(filePath), defaultData);
+// await db.read();
 
 // console.log(db.data);
 
 
 router.get("/", (req, res) => {
-  let allProducts = db.data.map((p) => {
-    const { id, name, priceArr, picNameArr } = p;
+  // let allProducts = db.data.map((p) => {
+  //   const { id, name, priceArr, picNameArr } = p;
 
-    return { id, name, priceArr, picNameArr };
-  });
-  if (!allProducts) {
-    return res.status(404).json({
-      status: "fail",
-      message: "找不到商品",
-    });
-  }
+  //   return { id, name, priceArr, picNameArr };
+  // });
+  // if (!allProducts) {
+  //   return res.status(404).json({
+  //     status: "fail",
+  //     message: "找不到商品",
+  //   });
+  // }
   res.status(200).json({
     status: "success",
     message: "找到全部商品!",
@@ -34,15 +34,15 @@ router.get("/", (req, res) => {
 });
 
 router.get("/detail/:id", (req, res)=>{
-  const id = req.params.id
-  let product = db.data.find((p, i)=>p.id == id)
-  if(!product){
-    res.status(404).json({
-      status:"fail",
-      message:"找不到商品"
-    })
-    return
-  }
+  // const id = req.params.id
+  // let product = db.data.find((p, i)=>p.id == id)
+  // if(!product){
+  //   res.status(404).json({
+  //     status:"fail",
+  //     message:"找不到商品"
+  //   })
+  //   return
+  // }
   res.status(200).json({
     status: "success",
     message: "找到指定的商品了!",

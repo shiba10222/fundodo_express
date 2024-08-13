cfe3eb1d5411df068ccd07a1e5b6c9fe2920bc12
RediChen <a0929052958@gmail.com>
2024-08-13 23:07:07 +0800

    更新格式與告示

--- db.js
+++ db.js
@@ -1,10 +1,8 @@
 import mysql from "mysql2/promise";
 import 'dotenv/config.js'
 
-//!重要：請將所使用的資料庫按以下設定
-//資料庫名稱: fundodo
-//使用者帳號: fundodo
-//使用者密碼: fdd12345
+//!公告
+//請在自己的電腦以 .env 檔設定以下五項資訊
 const conn = await mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,

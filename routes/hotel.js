11f039dc0b9b92d61e321a6c50fd03f025533770
ry <rouying0126@gmail.com>
2024-08-16 12:48:58 +0800

    更新旅館sql資料

--- routes/hotel.js
+++ routes/hotel.js
@@ -12,7 +12,13 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
-    const [result] = await conn.query("SELECT * FROM hotel");
+    const [result] = await conn.query(
+      ` SELECT h.*, GROUP_CONCAT(hi.path) AS images
+      FROM hotel h
+      LEFT JOIN hotel_img hi ON h.id = hi.hotel_id
+      GROUP BY h.id`
+    );
+    
     res.status(200).json({
       status: "success",
       message: "取得所有旅館",
@@ -33,7 +39,14 @@ router.get("/", async (req, res) => {
 router.get("/detail/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const [result] = await conn.query("SELECT * FROM hotel WHERE id = ?", [id]);
+    const [result] = await conn.query(
+      `
+     SELECT h.*, GROUP_CONCAT(hi.path) AS images
+    FROM hotel h
+    LEFT JOIN hotel_img hi ON h.id = hi.hotel_id
+    WHERE h.id = ?
+    GROUP BY h.id
+  `, [id]);
     
     if (result.length === 0) {
       return res.status(404).json({
@@ -49,7 +62,7 @@ router.get("/detail/:id", async (req, res) => {
       data: result[0]
     });
   } catch (error) {
-    console.error("Error fetching hotel details:", error);
+    console.error("找不到指定旅館", error);
     res.status(500).json({
       status: "error",
       message: "伺服器錯誤",
@@ -59,3 +72,5 @@ router.get("/detail/:id", async (req, res) => {
 });
 
 export default router;
+
+

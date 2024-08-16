59b27a4f9850b189bc356310a07a98a1fbf70d77
RediChen <a0929052958@gmail.com>
2024-08-16 09:24:28 +0800

    [fix] 改為讓使用函數的檔案提供其路徑

--- data/test/lib-json.js
+++ data/test/lib-json.js
@@ -10,24 +10,24 @@ const pathTo = pahtStr => resolve(import.meta.dirname, pahtStr);
 
 /**
  * 本函數會自動以 path.resolve 解析自檔案本身位置起始的路徑
- * @param path 檔案的路徑
+ * @param dirPath 執行之檔案的路徑
+ * @param path 欲讀取之檔案的路徑
  * @description 此函數為 promise 型態，使用時請記得使用 await
  */
-const readJson = async path => {
-  const content = await readFile(pathTo(path));
+const readJson = async (dirPath, path) => {
+  const content = await readFile(resolve(dirPath, path));
   return JSON.parse(content);
 }
 
-import { writeFile } from 'fs/promises';
-
 /**
  * 本函數會自動以 path.resolve 解析自檔案本身位置起始的路徑
- * @param path 檔案的路徑
+ * @param dirPath 執行之檔案的路徑
+ * @param path 欲存檔之檔案的路徑
  * @param dataStr 欲轉檔 json 的陣列或物件
  * @description 此函數為 promise 型態，使用時請記得使用 await
  */
-const writeJson = async (path, dataStr) => {
-  await writeFile(pathTo(path), JSON.stringify(dataStr))
+const writeJson = async (dirPath, path, dataStr) => {
+  await writeFile(resolve(dirPath, path), JSON.stringify(dataStr))
   .catch(err => err);
   console.log('已成功轉換成 json 檔');
 }

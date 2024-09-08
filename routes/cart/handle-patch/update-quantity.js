30e1e79a8fd8d1e918be9ec0ec9126b8b09be662
RediChen <a0929052958@gmail.com>
2024-09-09 00:26:35 +0800

    [fix] conn 改匯入方式

--- routes/cart/handle-patch/update-quantity.js
+++ routes/cart/handle-patch/update-quantity.js
@@ -1,6 +1,7 @@
+import conn from "../../../db.js";
 import { res200Json, res400Json } from '../../lib/common/response.js';
 
-export default async function (res, conn, cartID, newQty) {
+export default async function (res, cartID, newQty) {
   const [result] = await conn.query('SELECT buy_sort FROM `cart` WHERE id = ?', [cartID])
     if (result.length === 0) {
       res400Json(res, "刪除的請求成功地失敗了，查無指定的 cart ID");
